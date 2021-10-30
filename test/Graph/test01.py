260528300f0938bf0a881fbcce16f1940404f793
Fede <fedecasta@gmail.com>
2021-10-30 17:26:02 -0300

    Added test for DPF

--- test/Graph/test01.py
+++ test/Graph/test01.py
@@ -14,7 +14,36 @@ class TestEmptyGraph(unittest.TestCase):
         with self.assertRaises(ValueError):
             Graph(0).adjacents(1)
         
+    def test_v_cnt(self):
+        self.assertEqual( Graph(5).v_cnt , 5 )
+        self.assertEqual( len(Graph(5).vertices) , 5 )
 
+    def test_addEdges_index_error(self):
+        g = Graph(3)
+        with self.assertRaises(IndexError):
+            g.addEdge(0,4)
+        with self.assertRaises(IndexError):
+            g.addEdge(4,0)
+        
+    def test_addEdges_consitency(self):
+        g = Graph(3)
+        g.addEdge(0,1)
+        g.addEdge(0,2)
+        self.assertEqual( len(list(g.adjacents(0))) , 2 )
+        self.assertEqual( len(list(g.adjacents(1))) , 1 )
+        self.assertEqual( len(list(g.adjacents(2))) , 1 )
+
+    def test_addEdges_multi_edge(self):
+        g = Graph(3)
+        g.addEdge(0,1)
+        g.addEdge(0,1)
+        self.assertEqual( len(list(g.adjacents(0))) , 2 )
+        self.assertEqual( len(list(g.adjacents(1))) , 2 )
+
+    def test_addEdges_loop(self):
+        g = Graph(1)
+        g.addEdge(0,0)
+        self.assertEqual( len(list(g.adjacents(0))) , 2 )
 
 
 
--- test/Graph/test02.py
+++ test/Graph/test02.py
@@ -1,39 +1,55 @@
-from Graph.Graph import Graph
 import unittest
+
+from Graph.Graph import Graph
+from Graph.Paths import Paths
 
 class TestGraph(unittest.TestCase):
     
-    def test_v_cnt(self):
-        self.assertEqual( Graph(5).v_cnt , 5 )
-        self.assertEqual( len(Graph(5).vertices) , 5 )
-
-    def test_addEdges_index_error(self):
-        g = Graph(3)
-        with self.assertRaises(IndexError):
-            g.addEdge(0,4)
-        with self.assertRaises(IndexError):
-            g.addEdge(4,0)
-        
-    def test_addEdges_consitency(self):
-        g = Graph(3)
-        g.addEdge(0,1)
-        g.addEdge(0,2)
-        self.assertEqual( len(list(g.adjacents(0))) , 2 )
-        self.assertEqual( len(list(g.adjacents(1))) , 1 )
-        self.assertEqual( len(list(g.adjacents(2))) , 1 )
-
-    def test_addEdges_multi_edge(self):
-        g = Graph(3)
-        g.addEdge(0,1)
-        g.addEdge(0,1)
-        self.assertEqual( len(list(g.adjacents(0))) , 2 )
-        self.assertEqual( len(list(g.adjacents(1))) , 2 )
-
-    def test_addEdges_loop(self):
+    def test_dpf_single_node(self):
+    # Setup the graph
         g = Graph(1)
-        g.addEdge(0,0)
-        self.assertEqual( len(list(g.adjacents(0))) , 2 )
 
+        p = Paths( graph=g )
+
+        self.assertCountEqual( p.DepthFirstPath(0) , [0]  )
+
+    def test_depth_path_first(self):
+
+        # Setup the graph
+        g = Graph(7)
+        g.addEdge( 0 , 1 )
+        g.addEdge( 0 , 2 )
+        g.addEdge( 0 , 5 )
+        g.addEdge( 0 , 6 )
+        g.addEdge( 4 , 6 )
+        g.addEdge( 4 , 5 )
+        g.addEdge( 4 , 3 )
+        g.addEdge( 5 , 3 )
+    
+        p = Paths( graph=g )
+
+        self.assertCountEqual( p.DepthFirstPath(0) , list(range(7))  )
+
+    def test_dpf_ignore_sep(self):
+        # Setup the graph
+        g = Graph(10)
+        g.addEdge( 0 , 1 )
+        g.addEdge( 0 , 2 )
+        g.addEdge( 0 , 5 )
+        g.addEdge( 0 , 6 )
+        g.addEdge( 4 , 6 )
+        g.addEdge( 4 , 5 )
+        g.addEdge( 4 , 3 )
+        g.addEdge( 5 , 3 )
+        g.addEdge( 7 , 8 )
+        g.addEdge( 5 , 3 )
+        g.addEdge( 7 , 9 )
+    
+        p = Paths( graph=g )
+
+        self.assertCountEqual( p.DepthFirstPath(0) , list(range(7))  )
+        self.assertCountEqual( p.DepthFirstPath(7) , [ 7,8,9]  )
+        self.assertCountEqual( p.DepthFirstPath(8) , p.DepthFirstPath(9) )
 
 if __name__=='__main__':
     unittest.main()
